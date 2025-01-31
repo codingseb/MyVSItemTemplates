603893a622ff8ac367eb8d1589ba22e67c9800cf
Sébastien Geiser <SGeiser@precitrame.com>
2025-01-31 14:01:16 +0100

    Add DIResolve MarkupExtension and correct DIAutoFac Desc

---
+++ DIResolve/DIResolve.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using System;
+using System.Windows.Markup;
+
+namespace $rootnamespace$;
+
+/// <summary>
+/// A markup extension to resolve a type with DI
+/// </summary>
+public class DIResolve : MarkupExtension
+{
+    /// <summary>
+    /// The Type to resolve
+    /// </summary>
+    public Type ResolveType { get; set; }
+
+    /// <summary>
+    /// default constructor
+    /// </summary>
+    public DIResolve()
+    { }
+
+    /// <summary>
+    /// Constructor for direct type specification
+    /// </summary>
+    /// <param name="resolveType">The Type to resolve</param>
+    public DIResolve(Type resolveType)
+    {
+        ResolveType = resolveType;
+    }
+
+    /// <inheritdoc/>
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return DI.Container.Resolve(ResolveType);
+    }
+}
